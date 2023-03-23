50223a8480c1d42d3b5f96000fbfad19c0a6f076
hyb2419805602 <2419805602@qq.com>
2023-03-23 09:56:45 +0800

    home ubuntu16 ideaIC

---
+++ src/main/java/com/itheima/web/DeleteByIdServlet.java
@@ -0,0 +1,33 @@
+package com.itheima.web;
+
+import com.itheima.pojo.Brand;
+import com.itheima.service.BrandService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/deleteByIdServlet")
+public class DeleteByIdServlet extends HttpServlet {
+    private BrandService service = new BrandService();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+
+        service.deleteById(Integer.parseInt(id));
+
+        // 3. 转发到brand.jsp
+        request.getRequestDispatcher("/brand.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+
+    }
+}
